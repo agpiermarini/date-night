19d919efbb70852052627ca4439e89add115a90d
agpiermarini <piermarini.andrew@gmail.com>
2018-03-05 20:28:27 -0700

    moved comments to bottom of file

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,35 +8,6 @@ class BinarySearchTree
     @root = nil
   end
 
-  # def insert(score, title)
-  #   new_node = Node.new(score, title)
-  #   @root = new_node if @root.nil?
-  #   node = @root
-  #   depth = 0
-  #   until node.score == new_node.score
-  #     depth += 1
-  #     if new_node.score < node.score
-  #       node.left_node ||= new_node
-  #       node = node.left_node
-  #     elsif new_node.score > node.score
-  #       node.right_node ||= new_node
-  #       node = node.right_node
-  #     else
-  #       node = new_node
-  #     end
-  #   end
-  #   depth
-  # end
-
-  # def insert(score, title)
-  #   @root = Node.new(score, title) if @root.nil?
-  #   node = @root
-  #   until score == node.score
-  #     score < node.score ? (node = (node.left ||= Node.new(score, title))) : (node = (node.right ||= Node.new(score, title)))
-  #   end
-  #   depth_of(score)
-  # end
-
   def insert(score, title)
     @root = Node.new(score, title) if @root.nil?
     @root.insert(score, title)
@@ -62,14 +33,6 @@ class BinarySearchTree
       return false if node.nil?
     end
   end
-
-  # def max(node = @root)
-  #   node.right.nil? ? node : max(node.right)
-  # end
-
-  # def min(node = @root)
-  #   node.left.nil? ? node : min(node.left)
-  # end
 
   def max
     node = @root
@@ -112,3 +75,40 @@ class BinarySearchTree
     movies.length
   end
 end
+
+# def insert(score, title)
+#   new_node = Node.new(score, title)
+#   @root = new_node if @root.nil?
+#   node = @root
+#   depth = 0
+#   until node.score == new_node.score
+#     depth += 1
+#     if new_node.score < node.score
+#       node.left_node ||= new_node
+#       node = node.left_node
+#     elsif new_node.score > node.score
+#       node.right_node ||= new_node
+#       node = node.right_node
+#     else
+#       node = new_node
+#     end
+#   end
+#   depth
+# end
+
+# def insert(score, title)
+#   @root = Node.new(score, title) if @root.nil?
+#   node = @root
+#   until score == node.score
+#     score < node.score ? (node = (node.left ||= Node.new(score, title))) : (node = (node.right ||= Node.new(score, title)))
+#   end
+#   depth_of(score)
+# end
+
+# def max(node = @root)
+#   node.right.nil? ? node : max(node.right)
+# end
+
+# def min(node = @root)
+#   node.left.nil? ? node : min(node.left)
+# end
