3262cd18b245197a2eeff7ccfddc0a6fabbb895e
agpiermarini <piermarini.andrew@gmail.com>
2018-04-24 13:56:24 -0600

    Swap depth_of for recursive method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -14,14 +14,11 @@ class BinarySearchTree
     depth_of(score)
   end
 
-  def depth_of(score, node = @root)
-    depth = 0
-    until score == node.score
-      depth += 1
-      node = score < node.score ? node.left : node.right
-      return nil if node.nil?
-    end
-    depth
+  def depth_of(score, node = @root, depth = 0)
+    return nil if node.nil?
+    return depth if score == node.score
+    node = score < node.score ? node.left : node.right
+    depth_of(score, node, depth += 1)
   end
 
   def include?(score, node = @root)
@@ -61,66 +58,3 @@ class BinarySearchTree
     movies.length
   end
 end
-
-# def insert(score, title)
-#   new_node = Node.new(score, title)
-#   @root = new_node if @root.nil?
-#   node = @root
-#   depth = 0
-#   until node.score == new_node.score
-#     depth += 1
-#     if new_node.score < node.score
-#       node.left_node ||= new_node
-#       node = node.left_node
-#     elsif new_node.score > node.score
-#       node.right_node ||= new_node
-#       node = node.right_node
-#     else
-#       node = new_node
-#     end
-#   end
-#   depth
-# end
-
-# def insert(score, title)
-#   @root = Node.new(score, title) if @root.nil?
-#   node = @root
-#   until score == node.score
-#     score < node.score ? (node = (node.left ||= Node.new(score, title))) : (node = (node.right ||= Node.new(score, title)))
-#   end
-#   depth_of(score)
-# end
-#
-# def max
-#   node = @root
-#   loop do
-#     return node if node.right.nil?
-#     node = node.right
-#   end
-# end
-#
-# def min
-#   node = @root
-#   loop do
-#     return node if node.left.nil?
-#     node = node.left
-#   end
-# end
-#
-# def find_by_score(score)
-#   node = @root
-#   loop do
-#     return node if score == node.score
-#     score < node.score ? (node = node.left) : (node = node.right)
-#     return nil if node.nil?
-#   end
-# end
-#
-# def include?(score)
-#   node = @root
-#   loop do
-#     return true if score == node.score
-#     score < node.score ? (node = node.left) : (node = node.right)
-#     return false if node.nil?
-#   end
-# end
